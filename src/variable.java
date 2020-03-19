5b22b760d28cf74243cf3c1a2d7895e0a4b87e2d
badat <you@example.com>
2020-03-19 12:10:34 +0700

    variable

---
+++ src/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        float f = 10.2f;
+        System.out.println(f);
+        double d = 20.5;
+        System.out.println(d);
+        boolean b = true;
+        System.out.println(b);
+        char c = 'a';
+        System.out.println(c);
+        String s = "Hà Nội";
+        System.out.println(s);
+    }
+}
